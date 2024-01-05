521a8ba677cc4487a2563717664b29928c714b07
Jared Finder <jared@finder.org>
2024-01-04 16:59:28 -0800

    Initial stable release (0.1).
    The remaining concern blocking release was performance, but this has been
    running smoothly with my personal usage over the past three weeks.

--- README.md
+++ README.md
@@ -2,10 +2,24 @@
 [![GPL v3](https://img.shields.io/badge/license-GPL_v3-green.svg)](http://www.gnu.org/licenses/gpl-3.0.txt)
 [![Build Status](https://github.com/chaosemer/window-tool-bar/actions/workflows/test.yml/badge.svg?branch=main)](https://github.com/chaosemer/window-tool-bar/actions)
 
-Add tool bars to windows.  Clean up the default tool bar bindings.
-Make tool bars work in terminals.
+Add tool bars to windows.  Keep tool bars clean and useful for
+experienced Emacs users.
 
 ![A picture is worth a thousand words](images/screenshot.png)
+
+You may really like this package if any of the following is true:
+
+* You like tool bars, but find Emacs' default tool bar not helpful
+  because you already know how to do basic commands like open file,
+  save file, cut, copy, or paste.
+* You use the mouse when "browsing", like in help or info mode.
+* You use `mouse-autoselect-window` and would like to use tool bars.
+* You like how VSCode does tool bars.
+
+By putting a tool bar in each window, you can see multiple tool bars
+simultaneously directly next to the buffer the tool bar acts on.  This
+works especially well with in modes such as *info*, *help*, and
+*eww*.
 
 ## Installation
 
@@ -13,3 +27,27 @@ The easiest way to install window-tool-bar-mode is on GNU Emacs 29 via
 `package-vc-install`. Just run `M-x package-vc-install RET
 http://github.com/chaosemer/window-tool-bar-mode RET`. This installs
 the package locally.
+
+## Customization
+
+The default behavior is to make the per-window tab line show tool bars
+if the mode has a custom tool bar.  To enable this behavior either add
+`(global-window-tool-bar-mode 1)` to your init file or enable via
+customize via `M-x` `customize-group` `RET` `window-tool-bar` `RET`.
+If you want to enable for only specific modes, you can add
+`(window-tool-bar-mode 1)` to mode specific hooks.
+
+If you want to share the tool bar with an existing tab line, add
+`(:eval (window-tool-bar-string))` to `tab-line-format`.  You can also
+add this to `mode-line-format` or `header-line-format`.  This is
+recommended if you already are using the tab line for other things,
+like displaying tabs.
+
+NOTE: Support on text-only displays (terminals) is currently
+experimental so you probably want to wrap any enable logic behind a
+test for image display:
+
+``` emacs-lisp
+(when (display-images-p)
+  (global-window-tool-bar-mode 1))
+```
--- window-tool-bar.el
+++ window-tool-bar.el
@@ -3,7 +3,7 @@
 ;; Copyright 2023 Jared Finder
 ;; Author: Jared Finder <jared@finder.org>
 ;; Created: Nov 21, 2023
-;; Version: 0.1-beta
+;; Version: 0.1
 ;; Keywords: mouse
 ;; URL: http://github.com/chaosemer/window-tool-bar
 ;; Package-Requires: ((emacs "29.1"))
@@ -25,17 +25,30 @@
 
 ;;; Commentary:
 ;;
-;; This is in a very early state.  The eventual goals are as follows:
+;; This package puts a tool bar in each window.  This allows you to see
+;; multiple tool bars simultaneously directly next to the buffer it
+;; acts on which feels much more intuitive.  Emacs "browsing" modes
+;; generally have sensible tool bars, for example: *info*, *help*, and
+;; *eww* have them.
 ;;
-;; * Add tool bars to windows instead of frames.
-;; * Clean up the default tool bar bindings.
-;; * Make tool bars work in terminals.
+;; It does this while being mindful of screen real estate.  Most modes
+;; do not provide a custom tool bar, and this package does not show the
+;; default tool bar.  This means that for most buffers there will be no
+;; space taken up.  Furthermore, you can put this tool bar in the mode
+;; line or tab line if you want to share it with existing content.
+;;
+;; To get the default behavior, run (global-window-tool-bar-mode 1) or
+;; enable via M-x customize-group RET window-tool-bar RET.  This uses
+;; the per-window tab line to show the tool bar.
+;;
+;; If you want to share space with an existing tab line, mode line, or
+;; header line, add (:eval (window-tool-bar-string)) to
+;; `tab-line-format', `mode-line-format', or `header-line-format'.
 
 ;;; Todo:
 ;;
-;; Remaining work to hit 0.1:
-;;
-;; * Play with this on for a bit.
+;; Not all features planned are implemented yet.  Eventually I would
+;; like to also generally make tool bars better.
 ;;
 ;; Targeting 0.2:
 ;;
@@ -44,11 +57,11 @@
 ;;
 ;; Targeting 1.0:
 ;;
-;; * Clean up Emacs toolbars
-;;     * Default: Remove defaul tool-bar
+;; * Clean up Emacs tool bars
+;;     * Default: Remove default tool-bar entirely
 ;;     * grep, vc: Remove default tool-bar inherited
 ;;     * info: Remove Next / Prev / Up, which is already in the header
-;;     * smerge: Add toolbar for next/prev
+;;     * smerge: Add tool bar for next/prev
 ;;
 ;; Post 1.0 work:
 ;;
@@ -214,7 +227,7 @@ MENU-ITEM: Menu item to convert.  See info node (elisp)Tool Bar."
 
 ;;;###autoload
 (define-minor-mode window-tool-bar-mode
-  "Toggle display of the toolbar in the tab line of the current buffer."
+  "Toggle display of the tool bar in the tab line of the current buffer."
   :lighter nil
   (if (and window-tool-bar-mode
            (not (eq tool-bar-map (default-value 'tool-bar-map))))
