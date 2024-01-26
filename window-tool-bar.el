2c1cbce23946a70852e1b933ecb020fece07a7b4
Jared Finder <jared@finder.org>
2024-01-25 21:41:17 -0800

    Updating copyright, in prep for upstream to Emacs.

--- window-tool-bar.el
+++ window-tool-bar.el
@@ -1,6 +1,6 @@
 ;;; window-tool-bar.el --- Add tool bars inside windows -*- lexical-binding: t -*-
 
-;; Copyright 2023 Jared Finder
+;; Copyright (C) 2023-2024 Free Software Foundation, Inc.
 ;; Author: Jared Finder <jared@finder.org>
 ;; Created: Nov 21, 2023
 ;; Version: 0.2
@@ -8,20 +8,20 @@
 ;; URL: http://github.com/chaosemer/window-tool-bar
 ;; Package-Requires: ((emacs "29.1"))
 
-;; This file is not part of GNU Emacs.
+;; This file is part of GNU Emacs.
 
-;; This program is free software; you can redistribute it and/or modify
+;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
